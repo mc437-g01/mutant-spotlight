40d12b80105c71b5c0d3fd7db61c5f8555a8e8a5
Willians Zati <willzati@gmail.com>
2014-10-24 21:57:28 -0200

    LoadResultStory.java added

--- src/test/java/br/unicamp/ic/mc437/g1/acceptance/steps/loadresult/LoadResultSteps.java
+++ src/test/java/br/unicamp/ic/mc437/g1/acceptance/steps/loadresult/LoadResultSteps.java
@@ -1,18 +1,43 @@
 package br.unicamp.ic.mc437.g1.acceptance.steps.loadresult;
 
-import org.jbehave.core.annotations.Given;
-import org.jbehave.core.annotations.Pending;
-import org.jbehave.core.annotations.Then;
+import javax.annotation.Resource;
+
 import org.jbehave.core.annotations.*;
+import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+
+import br.unicamp.ic.mc437.g1.acceptance.steps.SharedSteps;
 public class LoadResultSteps {
-	@Then("the results of that xml file will be saved on the system")
-	@Pending
-	public void thenTheResultsOfThatXmlFileWillBeSavedOnTheSystem(){
-		 //TODO 
-	}
+	
+	private static final Logger log = LoggerFactory.getLogger(SharedSteps.class);
+
+	@Resource(name = "firefoxDriver")
+	private WebDriver driver;
+
+    @Value("classpath:acceptance/step_files/test_result_1.xml")
+    private org.springframework.core.io.Resource testResult1Resource;
+
+    @Value("${server.endpoint}")
+    private String serverEndpoint;
+
+    @Given("upload page loaded")
+    public void homePageLoaded() {
+        log.debug("homePageLoaded");
+
+        driver.navigate().to(serverEndpoint + "/new-result");
+    }
+	
 	@Given("that the user uploaded an xml file")
 	@Pending
 	public void givenThatTheUserUploadedAnXmlFile(){
+		 //TODO 
+	}
+	
+	@Then("the results of that xml file will be saved on the system")
+	@Pending
+	public void thenTheResultsOfThatXmlFileWillBeSavedOnTheSystem(){
 		 //TODO 
 	}
 
---
+++ src/test/java/br/unicamp/ic/mc437/g1/acceptance/story/LoadResultStory.java
@@ -0,0 +1,13 @@
+package br.unicamp.ic.mc437.g1.acceptance.story;
+
+import br.unicamp.ic.mc437.g1.acceptance.*;
+import org.junit.runner.*;
+import org.springframework.test.context.junit4.*;
+
+/**
+ * @author Willians Zati (will.zati@bb.com.br)
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@AcceptanceTest
+public class LoadResultStory extends AbstractSpringJBehaveStory {
+}
