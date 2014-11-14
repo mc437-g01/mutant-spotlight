8a453cbf6cf831f1b52eb634b88e72359ae1f755
Carlos Gregoreki <cgregoreki@gmail.com>
2014-11-14 20:14:51 -0200

    new steps for graph viewing at testresults report

--- src/test/java/br/unicamp/ic/mc437/g1/acceptance/steps/visualizegraph/VisualizeGraphSteps.java
+++ src/test/java/br/unicamp/ic/mc437/g1/acceptance/steps/visualizegraph/VisualizeGraphSteps.java
@@ -10,18 +10,24 @@ import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Pending;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import br.unicamp.ic.mc437.g1.acceptance.Steps;
-import br.unicamp.ic.mc437.g1.acceptance.steps.clickupload.ClickUploadButtonSteps;
 import br.unicamp.ic.mc437.g1.acceptance.story.VisualizeGraphStory;
+import br.unicamp.ic.mc437.g1.acceptance.steps.viewresult.ViewResultSteps;
+import br.unicamp.ic.mc437.g1.entity.TestResult;
+import br.unicamp.ic.mc437.g1.model.dao.TestResultDAO;
 
 @Steps
 public class VisualizeGraphSteps {
 	@Resource(name = "firefoxDriver")
     private WebDriver driver;
+	
+	private TestResultDAO testResultDao;
 	
 	private static final Logger log = LoggerFactory.getLogger(VisualizeGraphStory.class);
 	
@@ -32,9 +38,22 @@ public class VisualizeGraphSteps {
 	}
 
 	@Then("a graph of the test set score shoud be shown")
-	@Pending
 	public void thenAGraphOfTheTestSetScoreShoudBeShown() {
 		// TODO
+		WebElement grafico;
+		try {
+			//try to find the graph 
+			grafico = driver.findElement(By.id("test-result-set-graph"));
+			//everything was fine. it found the graph. 
+			//trivial assertEquals.
+			assertEquals(1,1);
+		} catch(Exception E) {
+			//nope. it didn't find a graph.
+			grafico = null;
+			//trivial false assertEquals.
+			assertEquals(1,0);
+		}
+			
 	}
 
 	@Given("that the user has access to the system")
@@ -42,17 +61,25 @@ public class VisualizeGraphSteps {
 	public void givenThatTheUserHasAccessToTheSystem() {
 		// TODO
 	}
-
+	
 	@Given("a test set has been upload")
-	@Pending
 	public void givenATestSetHasBeenUpload() {
+		try {
+			(new ViewResultSteps()).givenResultsInTheSystem();
+		} catch(Exception E){
+			E.printStackTrace();
+		}
 		//TODO
 	}
 
 	@Given("the uploaded file is $file")
-	@Pending
 	public void givenTheUploadedFileIsTest_result_1xml(String filename) {
-		// TODO
+		
+		List<TestResult> lista = testResultDao.list();
+		//get the last testResult inserted
+		TestResult testResult = lista.get(lista.size() -1);
+		//get its filename and compare
+		assertEquals(filename, testResult.getName()); 
 	}
 
 	@Then("the graph of the Test Case will be 100% green")
@@ -70,6 +97,7 @@ public class VisualizeGraphSteps {
 	@When("the user visualizes the report of the uploaded file")
 	@Pending
 	public void whenTheUserVisualizesTheReportOfTheUploadedFile() {
-		// TODO
+		// TODO		
+		
 	}
 }
